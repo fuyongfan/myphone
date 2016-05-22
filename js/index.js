07029d9bb91cede402fd20d221ce51c5ce39c25c
fuyongfan <526332289@qq.com>
2016-05-22 09:53:30 +0800

    0522

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 var main = document.querySelector("#main");
 var oLis = document.querySelectorAll("#main>ul>li");
 var winW = document.documentElement.clientWidth;
@@ -9,54 +10,42 @@ if (winW / winH <= desW / desH) {
 } else {
     main.style.webkitTransform = "scale(" + winW / desW + ")";
 }
-$(document).on('touchmove', function (ev) {
-    ev.preventDefault();
-});
 $(function () {
-    var viewHeight = $(window).height();
+    var viewHeight = $("body").height();
     var $li = $('#list').find('>li');
+    //$('#main').css('height', viewHeight);
     slidePage();
     function slidePage() {
         var startY = null;
         var step = 1 / 4;
         var nowIndex = 0;
-        var nextorprevIndex = 0;
+        var nextOrPrevIndex = 0;
         var bOk = true;
-        var n = 0;
+        //绑定touchstart事件
         $li.on('touchstart', function (ev) {
             if (bOk == false) return;
+
             bOk = false;
             var touch = ev.originalEvent.touches[0];
             startY = touch.pageY;
             nowIndex = $(this).index();
             $li.on('touchmove.move', function (ev) {
-                n++;
                 var touch = ev.originalEvent.touches[0];
                 $(this).siblings('li').hide();
                 if (touch.pageY < startY) {//up
-                    nextorprevIndex = nowIndex == $li.length - 1 ? 0 : nowIndex + 1;
-                    $li.eq(nextorprevIndex).css('transform', 'translate(0,' + (viewHeight + touch.pageY - startY) + 'px)')
+                    nextOrPrevIndex = nowIndex == $li.length - 1 ? 0 : nowIndex + 1;
+                    $li.eq(nextOrPrevIndex).css('transform', 'translate(0,' + (viewHeight + touch.pageY - startY) + 'px)')
                 } else if (touch.pageY > startY) {//down
-                    nextorprevIndex = nowIndex == 0 ? $li.length - 1 : nowIndex - 1;
-                    $li.eq(nextorprevIndex).css('transform', 'translate(0,' + (-viewHeight + touch.pageY - startY) + 'px)')
+                    nextOrPrevIndex = nowIndex == 0 ? $li.length - 1 : nowIndex - 1;
+                    $li.eq(nextOrPrevIndex).css('transform', 'translate(0,' + (-viewHeight + touch.pageY - startY) + 'px)')
                 } else {
                     bOk = true;
                 }
-                $(this).css('transform', 'translate(0,' + (touch.pageY - startY) * step + 'px) scale(' + (1 - Math.abs(touch.pageY - startY) / viewHeight * step) + ')');
-                $li.eq(nextorprevIndex).addClass('zIndex').show();
+                $li.eq(nextOrPrevIndex).addClass('zIndex').show();
             });
             $li.on('touchend.move', function (ev) {
-                if (n == 0)return;
-                var touch = ev.originalEvent.changedTouches[0];
-                if (touch.pageY < startY) {//up
-                    $(this).css('transform', 'translate(0,' + (-viewHeight * step) + 'px) scale(' + (1 - step) + ')')
-                } else if (touch.pageY > startY) {//down
-                    $(this).css('transform', 'translate(0,' + (viewHeight * step) + 'px) scale(' + (1 - step) + ')')
-                } else {
-                    bOk = true;
-                }
-                $li.eq(nextorprevIndex).css('transform', 'translate(0,0)');
-                $li.eq(nextorprevIndex).css('transition', '.3s');
+                $li.eq(nextOrPrevIndex).css('transform', 'translate(0,0)');
+                $li.eq(nextOrPrevIndex).css('transition', '.3s');
                 $li.eq(nowIndex).css('transition', '.3s');
                 $li.off('.move');
             })
@@ -68,27 +57,26 @@ $(function () {
         function resetFn() {
             $li.css('transform', '');
             $li.css('transition', '');
-            $li.eq(nextorprevIndex).removeClass('zIndex').siblings('li').hide();
+            $li.eq(nextOrPrevIndex).removeClass('zIndex').siblings('li').hide();
             bOk = true;
         }
     }
-});$(function(){
-    var musicBtn = document.getElementById("musicBtn");
-    var musicAudio = document.getElementById("audio1");
-    window.addEventListener("load", function () {
-        musicAudio.play();
-        musicAudio.addEventListener("canplay", function () {
-            musicBtn.style.display = "block";
-            musicBtn.className = "music move";
-        }, false);
-        musicBtn.addEventListener("touchend", function () {
-            if (musicAudio.paused) {
-                musicAudio.play();
-                musicBtn.className = "music move";
-            } else {
-                musicAudio.pause();
-                musicBtn.className = "music";
-            }
-        }, false);
-    }, false);
 });
+var musicBtn = document.getElementById("musicBtn");
+var musicAudio = document.getElementById("audio1");
+window.addEventListener("load", function () {
+    musicAudio.play();
+    musicAudio.addEventListener("canplay", function () {
+        musicBtn.style.display = "block";
+        musicBtn.className = "music move";
+    }, false);
+    musicBtn.addEventListener("touchend", function () {
+        if (musicAudio.paused) {
+            musicAudio.play();
+            musicBtn.className = "music move";
+        } else {
+            musicAudio.pause();
+            musicBtn.className = "music";
+        }
+    }, false);
+}, false);
